478e960866d2d80547bc3668ebb3db823067f9a4
Onwuka Gideon <dongidomed@gmail.com>
2018-02-01 19:35:48 +0100

    Update CreateToken.java

--- src/main/java/com/tchatapp/chats/twilio/CreateToken.java
+++ src/main/java/com/tchatapp/chats/twilio/CreateToken.java
@@ -6,11 +6,11 @@ import com.twilio.jwt.accesstoken.ChatGrant;
 public class CreateToken {
 
 	  public static String token(String identity) {
-		    String twilioAccountSid = "AC1f7d11ebe4fcc2cb4c2a1dfa6a661f97";
-		    String twilioApiKey = "SK6997daff821400758519588e18d4be7e";
-		    String twilioApiSecret = "D6GgjcOUHkuDqB8dkNlFxZq9wV9n03P7";
+		    String twilioAccountSid = "AC****************************";
+		    String twilioApiKey = "SK********************************";
+		    String twilioApiSecret = "D6G****************************";
 
-		    String serviceSid = "ISf8e723460c2d4c5ca88897e97cf31ea4";
+		    String serviceSid = "IS**********************************";
 		    String deviceId = "browser";
 		    String appName = "chatX";
 		    String endpointId = appName + ":" + identity + ":" + deviceId;
